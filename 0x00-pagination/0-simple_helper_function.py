ecf04ad9ee23eb38262b75cd505c3700b0e859b0
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2023-08-20 11:22:19 -0400

    task0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
-def index_range(page, page_size: int):
-    """ create index_range """
-    last = page * page_size
-    return (page - 1) * page_size, last
+"""
+File: 0-simple_helper_function.py
+Desc: This module contains a python code related to backend
+      pagination.
+Author: Gizachew Bayness
+Date Created: Feb 20, 2022
+"""
+
+
+def index_range(page, page_size):
+    """Helper function"""
+    last_page = page_size * page
+    return (last_page - page_size, las
