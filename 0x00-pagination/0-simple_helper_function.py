04ddba69b25791f83452cbfc21dc8dfc619fae21
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2023-08-20 10:54:34 -0400

    task0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-def index_range(page, page_size)->tuple:
+def index_range(page: int, page_size: int) -> tuple:
     """ create index_range """
     return (page - 1) * page_size, page * page_size
