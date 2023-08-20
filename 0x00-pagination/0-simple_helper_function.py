685d8e0da91287e15993a1df1fcebaf20a90024d
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2023-08-20 11:34:33 -0400

    task0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,5 +6,4 @@ File: 0-simple_helper_function.py
 
 def index_range(page, page_size):
     """Helper function"""
-    last = page_size * page
-    return (page - 1) * page_size, last
+    return (page - 1) * page_size, page * page_size
