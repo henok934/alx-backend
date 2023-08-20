59512119eb4b2fee54ce0ffffe639fcbb32b14dd
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2023-08-20 11:20:01 -0400

    task0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 def index_range(page, page_size: int):
     """ create index_range """
-    return (page - 1) * page_size, page * page_size
+    last = page * page_size
+    return (page - 1) * page_size, last
